1f5cd3b7dc447068c6848e54317e61a965286ca9
Ethan <ewtaylor@mymail.mines.edu>
2019-11-21 10:19:48 -0700

    Added LoadFile function

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,11 @@
+#   Ethan Taylor
+#  ​CSCI 102 – Section C
+#   Week 11 - Part A
+
 def PrintOutput(str):
     print("OUTPUT", str)
+
+def LoadFile(path):
+    file = open(path, r)
+    print ("OUTPUT" + file.read().split("\n")
+    file.close()
